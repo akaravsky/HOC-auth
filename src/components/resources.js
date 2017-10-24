8a4e1e0e943e9fac9c9950bf16dd2d4a5bba81e5
Alexandr Karavsky <askaravskiy@gmail.com>
2017-10-24 17:07:24 +0300

    first

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Secret Recipe
+      <ul>
+        <li>Cup Sugar</li>
+        <li>Cup Paper</li>
+        <li>Cup Salt</li>
+      </ul>
+    </div>
+  );
+};
